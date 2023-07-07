409b71de9e524840fa6cc22b28ae73671433d1a9
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-07-07 19:13:35 +1200

    build: don't commit package.json changes from build process

--- scripts/pre-release.ts
+++ scripts/pre-release.ts
@@ -1,10 +1,6 @@
-import childProcess from "node:child_process";
 import fs from "node:fs/promises";
-import util from "node:util";
 
 import pkg from "../package.json" assert { type: "json" };
-
-const exec = util.promisify(childProcess.exec);
 
 if (
   process.env["CI"] === undefined ||
@@ -37,6 +33,3 @@ const releasePkg = Object.fromEntries(
 await fs.writeFile("./package.json", JSON.stringify(releasePkg, undefined, 2), {
   encoding: "utf8",
 });
-
-await exec("git add package.json");
-await exec('git commit -m "build: package.json"');
