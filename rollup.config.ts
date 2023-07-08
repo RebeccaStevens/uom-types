363ca33e87a29aa46f1599eb2cab47d362c53b08
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-07-08 21:23:50 +1200

    build: use relative imports in dist files

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,3 +1,6 @@
+import assert from "node:assert/strict";
+import path from "node:path";
+
 import rollupPluginTypescript from "@rollup/plugin-typescript";
 import { defineConfig, type Plugin } from "rollup";
 import rollupPluginAutoExternal from "rollup-plugin-auto-external";
@@ -33,6 +36,35 @@ const common = defineConfig({
     moduleSideEffects: [],
     propertyReadSideEffects: false,
     unknownGlobalSideEffects: false,
+  },
+
+  external: (
+    source: string,
+    importer: string | undefined,
+    isResolved: boolean,
+  ) => {
+    if (!isResolved || importer === undefined) {
+      return null;
+    }
+
+    assert(path.isAbsolute(source));
+    const relativeSource = path.relative(process.cwd(), source);
+    const relativeImporter = path.relative(process.cwd(), importer);
+
+    if (!relativeSource.startsWith("src/")) {
+      return null;
+    }
+
+    const sourceSubproject = relativeSource.slice(
+      4,
+      Math.max(0, relativeSource.indexOf("/", 4)),
+    );
+    const importerSubproject = relativeImporter.slice(
+      4,
+      Math.max(0, relativeImporter.indexOf("/", 4)),
+    );
+
+    return sourceSubproject !== importerSubproject;
   },
 });
 
