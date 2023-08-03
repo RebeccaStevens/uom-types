a0205bd1ebaf56951bd68f03516ad28fd3cbe3b1
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-08-03 19:50:01 +1200

    fix: index not exporting files

--- src/units/converters/index.ts
+++ src/units/converters/index.ts
@@ -1,1 +1,9 @@
-export {};
+export * from "./angle";
+export * from "./area";
+export * from "./duration";
+export * from "./energy";
+export * from "./frequency";
+export * from "./mass";
+export * from "./prefixes";
+export * from "./temperature";
+export * from "./volume";
