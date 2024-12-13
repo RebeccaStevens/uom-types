5f3f37a839dfec07cd22dd05bc9a15da227e48e4
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2024-12-13 15:24:32 +1300

    feat: expose more entry points

--- src/converters/volume.ts
+++ src/converters/volume.ts
@@ -1,7 +1,7 @@
-import type { UnitConversionRate } from "../../core.ts";
-import { div, mul } from "../../math/index.ts";
-import type { Liter, Meter } from "../common/index.ts";
-import type { Cubic } from "../modifiers/index.ts";
+import type { UnitConversionRate } from "../core.ts";
+import { div, mul } from "../math/index.ts";
+import type { Liter, Meter } from "../units/common/index.ts";
+import type { Cubic } from "../units/modifiers/index.ts";
 
 /**
  * Convert {@link Cubic}<{@link Meter}> to {@link Liter}.
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,4 @@
-export type {
-  AbstractUnit,
-  AbstractUnitFrom,
-  Unit,
-  UnitClass,
-  UnitConversionRate,
-  UnitConversionRateFrom,
-  UnitFrom,
-  UnitMeta,
-  UnitSubvalues,
-  UnknownAbstractUnit,
-  UnknownUnit,
-  UnknownUnitClass,
-  UnknownUnitConversionRate,
-  UnknownUnitMeta,
-} from "./core.ts";
-export type {
-  DivideExponents,
-  Exponent,
-  MultiplyExponents,
-  NegateExponent,
-  NegExponent,
-  PosExponent,
-  SubExponents,
-  SumExponents,
-} from "./exponents.ts";
-export type {
-  Divide,
-  DivideUnitSubvalues,
-  Inverse,
-  InverseUnitSubvalues,
-  Multiply,
-  MultiplyUnitSubvalues,
-  Pow,
-  PowUnitSubvalues,
-  Root,
-  RootUnitSubvalues,
-} from "./units-operations/index.ts";
-
 export * from "./math/index.ts";
+export * from "./types.ts";
 export * from "./units/index.ts";
+export * from "./converters/index.ts";
